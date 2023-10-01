780f3fba5317de458e7c13a7e3f568ac2e8f8edb
Lohan <ltwm1998@gmail.com>
2023-10-01 16:45:29 -0400

    update labs

--- index.js
+++ index.js
@@ -4,22 +4,24 @@
 //     console.log(`I'm ${age} years old. Happy birthday to me!`);
 // }
 
-// const gifts = ["teddy bear", "drone", "doll"];
+const gifts = ["teddy bear", "drone", "doll"];
 
-// function wrapGifts(gifts) {
-//     for (let i = 0; i < gifts.length; i++) {
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     }
-//     return gifts;
-// }
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  
+    return gifts;
+  }
 
-// wrapGifts(gifts);
+wrapGifts(gifts);
 
 
 function writeCards(array, event) {
     const myArray = [];
     for (let i = 0; i < array.length; i++) {
-        myArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);  
+        myArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        debugger;    
     } 
     return myArray;
 }
